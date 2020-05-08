1c2f86373aa2925468bba7717dde509b67b39385
Mayank <himanshu@Mayanks-MacBook-Pro.local>
2020-05-08 13:27:05 +0530

    favicon added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Atomize Design System`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `The most advanced UI design framework for Sketch and Figma`,
     author: `@akashtyagi`,
   },
   plugins: [
