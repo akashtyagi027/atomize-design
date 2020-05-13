121ee9e624ba5a66d6f4c239484a54b6bbf612ec
Mayank <himanshu@Mayanks-MacBook-Pro.local>
2020-05-13 12:33:07 +0530

    meta tags chnges

--- src/components/layout.js
+++ src/components/layout.js
@@ -76,21 +76,6 @@ const Layout = ({ children }) => {
             }}
           />
 
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `"use strict";
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:1808029,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              ;`,
-            }}
-          />
-
           {/* Drift Code */}
           <script
             dangerouslySetInnerHTML={{
